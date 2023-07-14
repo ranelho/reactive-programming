e8d9b577c9ecf2e6454955d6b71f48e851f1d4e7
ranelho <ranelhol@gmail.com>
2023-07-14 13:33:42 -0300

    add integration test endpoint save

---
+++ src/test/java/com/rlti/webflux/controller/impl/UserControllerImplTest.java
@@ -0,0 +1,64 @@
+package com.rlti.webflux.controller.impl;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.rlti.webflux.entity.User;
+import com.rlti.webflux.mapper.UserMapper;
+import com.rlti.webflux.model.request.UserRequest;
+import com.rlti.webflux.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.BodyInserters.fromValue;
+import static reactor.core.publisher.Mono.just;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+class UserControllerImplTest {
+
+    private static final String ID = "123456";
+    private static final String NAME = "Valdir";
+    private static final String EMAIL = "valdir@mail.com";
+    private static final String PASSWORD = "123";
+    private static final String BASE_URI = "/users";
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private UserService service;
+
+    @MockBean
+    private UserMapper mapper;
+
+    @MockBean
+    private MongoClient mongoClient;
+
+
+    @Test
+    @DisplayName("Test endpoint save with success")
+    void testSaveWithSuccess() {
+        final var request = new UserRequest(NAME, EMAIL, PASSWORD);
+
+        when(service.save(any(UserRequest.class))).thenReturn(just(User.builder().build()));
+
+        webTestClient.post().uri(BASE_URI)
+                .contentType(APPLICATION_JSON)
+                .body(fromValue(request))
+                .exchange()
+                .expectStatus().isCreated();
+
+        verify(service).save(any(UserRequest.class));
+    }
+}
