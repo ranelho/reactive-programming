614c7184751e86324d00dbc0e50e4aad4cdc7e50
ranelho <ranelhol@gmail.com>
2023-07-14 10:54:49 -0300

    add unit test save

---
+++ src/test/java/com/rlti/webflux/service/UserServiceTest.java
@@ -0,0 +1,54 @@
+package com.rlti.webflux.service;
+
+import com.rlti.webflux.entity.User;
+import com.rlti.webflux.mapper.UserMapper;
+import com.rlti.webflux.model.request.UserRequest;
+import com.rlti.webflux.repository.UserRepository;
+import com.rlti.webflux.service.exception.ObjectNotFoundException;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import java.util.Objects;
+
+import static java.lang.String.format;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserServiceTest {
+
+    @Mock
+    private UserRepository repository;
+
+    @Mock
+    private UserMapper mapper;
+
+    @InjectMocks
+    private UserService service;
+
+    @Test
+    void testSave() {
+        UserRequest request = new UserRequest("ranelho", "ran@mail.com", "123");
+        User entity = User.builder().build();
+
+        when(mapper.toEntity(any(UserRequest.class))).thenReturn(entity);
+        when(repository.save(any(User.class))).thenReturn(Mono.just(User.builder().build()));
+
+        Mono<User> result = service.save(request);
+
+        StepVerifier.create(result)
+                .expectNextMatches(user -> user.getClass() == User.class)
+                .expectComplete()
+                .verify();
+
+        Mockito.verify(repository, times(1)).save(any(User.class));
+    }
+
+
+}
